926a7e80cd5491abcd5f111e957a17d48f3bec18
Henrik Gustavii <henrik@gustavii.com>
2017-10-21 08:27:21 +0200

    Added collapsing and expanding for the text entry field. Added saving message data to firebase db.

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 import Firebase
 
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate  {
+    
     
     // Declare instance variables here
-
+    
     
     // We've pre-linked the IBOutlets
     @IBOutlet var heightConstraint: NSLayoutConstraint!
@@ -27,19 +28,25 @@ class ChatViewController: UIViewController {
         super.viewDidLoad()
         
         //TODO: Set yourself as the delegate and datasource here:
-        
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
         
         
         //TODO: Set yourself as the delegate of the text field here:
-
+        messageTextfield.delegate = self
         
         
         //TODO: Set the tapGesture here:
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
         
         
 
         //TODO: Register your MessageCell.xib file here:
-
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        
+        // Calls the "cell settings" for larger text view
+        configureTableView()
         
     }
 
@@ -50,19 +57,42 @@ class ChatViewController: UIViewController {
     
     
     //TODO: Declare cellForRowAtIndexPath here:
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        
+        let messageArray = ["First message", "Second message", "Third message"]
+        
+        cell.messageBody.text = messageArray[indexPath.row]
+        
+        return cell
+        
+    }
     
     
     //TODO: Declare numberOfRowsInSection here:
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 3
+        
+    }
     
     
     //TODO: Declare tableViewTapped here:
+    @objc func tableViewTapped() {
+        
+        messageTextfield.endEditing(true)
+        
+    }
     
     
-    
-    //TODO: Declare configureTableView here:
-    
+    //TODO: Declare configureTableView here: (in order to make sure that cells can have a larger size to fit text)
+    func configureTableView() {
+        
+        messageTableView.rowHeight = UITableViewAutomaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+        
+    }
     
     
     ///////////////////////////////////////////
@@ -72,13 +102,30 @@ class ChatViewController: UIViewController {
     
 
     
-    //TODO: Declare textFieldDidBeginEditing here:
+    //TODO: Declare textFieldDidBeginEditing here: (gets triggered automatically when pressed inside textfield)
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.4) {
+            
+            self.heightConstraint.constant = 308
+            self.view.layoutIfNeeded()
+            
+        }
+    }
     
     
     
-    
-    //TODO: Declare textFieldDidEndEditing here:
-    
+    //TODO: Declare textFieldDidEndEditing here: (wont be triggered unless there's a tap gesture registered outside the textfield that triggers this method)
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.4) {
+            
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+            
+        }
+        
+    }
 
     
     ///////////////////////////////////////////
@@ -92,9 +139,33 @@ class ChatViewController: UIViewController {
     
     @IBAction func sendPressed(_ sender: AnyObject) {
         
+        messageTextfield.endEditing(true)
         
         //TODO: Send the message to Firebase and save it in our database
+        messageTextfield.isEnabled = false
+        sendButton.isEnabled = false
         
+        let messagesDB = Database.database().reference().child("Messages")
+        let messageDictionary = ["Sender": Auth.auth().currentUser?.email, "MessageBody": messageTextfield.text]
+        
+        messagesDB.childByAutoId().setValue(messageDictionary) {
+            (error, ref) in
+            
+            if error != nil {
+                
+                print(error)
+                
+            } else {
+                
+                print("Message saved successfully!")
+                
+                self.messageTextfield.isEnabled = true
+                self.sendButton.isEnabled = true
+                
+                self.messageTextfield.text = ""
+                
+            }
+        }
         
     }
     
